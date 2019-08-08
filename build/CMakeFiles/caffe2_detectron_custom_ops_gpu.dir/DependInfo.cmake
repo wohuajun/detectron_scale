91ef3c2e13e098738d811caf1ca56723bb921e11
wohuajun <whjinc@163.com>
2019-08-08 18:50:20 +0800

    Add files via upload

---
+++ build/CMakeFiles/caffe2_detectron_custom_ops_gpu.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/icubic/detectron/detectron/ops/zero_even_op.cc" "/home/icubic/detectron/build/CMakeFiles/caffe2_detectron_custom_ops_gpu.dir/detectron/ops/zero_even_op.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AT_PARALLEL_OPENMP=1"
+  "_THP_CORE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/icubic/pytorch/torch/lib/include"
+  "/usr/local/cuda/include"
+  "../-I/home/icubic/pytorch/torch/include"
+  "/home/icubic/pytorch/torch/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
